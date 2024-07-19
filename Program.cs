03c54429c578b47cd45d668bd931d2a8c0186880
7oolguy <yeast1242@gmail.com>
2024-07-19 12:07:05 -0300

    Final Project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using ProjectChallange.Models;
+
+Console.WriteLine("Digite Numero de telephone:");
+string numero = Console.ReadLine();
+
+Console.WriteLine("Digite Modelo do telephone:");
+string modelo = Console.ReadLine();
+
+Console.WriteLine("Digite IMEI do telephone:");
+string imei = Console.ReadLine();
+
+Console.WriteLine("Digite Memoria do telephone:");
+int memoria = int.Parse(Console.ReadLine());
+
+Iphone iphone = new Iphone(numero, modelo, imei, memoria);
+iphone.Ligar();
+iphone.ReceberLigacao();
+Console.WriteLine("Digite Nome do App para instalar:");
+string app = Console.ReadLine();
+iphone.InstalarAplicativo(app);
+
+Nokia nokia = new Nokia(numero, modelo, imei, memoria);
+nokia.Ligar();
+nokia.ReceberLigacao();
+Console.WriteLine("Digite Nome do App para instalar:");
+app = Console.ReadLine();
+nokia.InstalarAplicativo(app);
